0e7e8d49d1b71f395cf9f238b979aab48c0d4a05
ravis <ravissavoy49@gmail.com>
2022-04-01 09:09:49 -0500

    commented code

--- raygame/StateMachineComponent.cpp
+++ raygame/StateMachineComponent.cpp
@@ -32,37 +32,46 @@ void StateMachineComponent::update(float deltaTime)
 	MathLibrary::Vector2 ownerPos = getOwner()->getTransform()->getWorldPosition();
 	float distanceFromTarget = (targetPos - ownerPos).getMagnitude();
 
+	//The distances from the target based on different ranges
 	bool targetInRange = distanceFromTarget <= m_seekRange;
 	bool targetInPathRange = distanceFromTarget <= m_pathFindRange;
 
 	switch (m_currentState)
 	{
 	case PATHFIND:
+		//Sets the steering forces 
 		m_seekComponent->setSteeringForce(0);
 		m_wanderComponent->setSteeringForce(0);
 		m_pathFindComponent->setEnabled(true);
 
+		//only steps into wander if its not in range
 		if (!targetInRange)
 			setCurrentState(WANDER);
+		//only steps into seek if it is in the range
 		if (targetInPathRange)
 			setCurrentState(SEEK);
 		break;
 	case WANDER:
+		//Sets the steering forces 
 		m_seekComponent->setSteeringForce(30);
 		m_wanderComponent->setSteeringForce(m_wanderForce);
 		m_pathFindComponent->setEnabled(false);
 
+		//only steps into pathFind if it is in the range
 		if (targetInPathRange)
 			setCurrentState(PATHFIND);
+		//only steps into seek if it is in the range
 		if(targetInRange)
 			setCurrentState(SEEK);
 
 		break;
 	case SEEK:
+		//Sets the steering forces 
 		m_seekComponent->setSteeringForce(m_seekForce);
 		m_wanderComponent->setSteeringForce(0);
 		m_pathFindComponent->setEnabled(false);
 
+		//only steps into pathfind if its not in range
 		if (!targetInPathRange)
 			setCurrentState(PATHFIND);
 		break;
